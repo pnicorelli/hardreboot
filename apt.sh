c241f7b1789de14c0d6adf60919ce861c978a43c
Paolo -Willy- Nicorelli <p.nicorelli@gmail.com>
2016-04-05 20:05:11 +0200

    init

---
+++ apt.sh
@@ -0,0 +1,19 @@
+apt-get update
+
+## TOOLS
+apt-get install -y vim curl build-essential
+
+apt-get install -y nmap htop filezilla geany vlc geeqie
+
+
+## SERVICES
+apt-get install -y mongodb apache2
+
+## APACHE CONF
+a2enmod userdir
+
+## APPS
+
+
+##
+apt-get install -y network-manager-openvpn-gnome cups-pdf
---
+++ download.sh
@@ -0,0 +1,23 @@
+## RUN THIS AS ROOT
+
+cd /tmp/
+
+## ATOM EDITOR
+curl -L https://atom.io/download/deb > atom.deb
+dpkg -i atom.deb
+
+
+## NODEJS (with n to avoid versioning issue)
+curl -sL https://deb.nodesource.com/setup_0.10 | bash -
+apt-get install -y nodejs
+npm install -g n
+sudo npm install -g bower
+sudo npm install -g gulp
+
+## CHROME
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
+
+
+## HEROKU
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
